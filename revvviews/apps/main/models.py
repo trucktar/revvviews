0038b313f8f087b07a61462d3619b396c1fb3c44
trucktar <57882685+trucktar@users.noreply.github.com>
2020-07-12 23:10:51 +0300

    Alter review fields and add timestamp to project

--- revvviews/apps/main/models.py
+++ revvviews/apps/main/models.py
@@ -13,12 +13,21 @@ class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    @property
+    def username(self):
+        return self.user.username
+
+    @classmethod
+    def get_by_username(cls, username):
+        return cls.objects.get(user__username=username)
+
 
 class Project(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=185)
     screenshot = models.ImageField(upload_to='uploads')
     live_link = models.URLField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     profile = models.ForeignKey(
         Profile,
