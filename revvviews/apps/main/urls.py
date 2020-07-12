ee2ad632bfa21e6c9de745fdc11694d581eac7c9
trucktar <57882685+trucktar@users.noreply.github.com>
2020-07-12 15:54:46 +0300

    Configure auth views and add URL mappings

--- revvviews/apps/main/urls.py
+++ revvviews/apps/main/urls.py
@@ -1,9 +1,19 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
 from django.urls import path
+from django_registration.backends.one_step.views import RegistrationView
 
 from revvviews.apps.main import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('register/',
+         RegistrationView.as_view(
+             template_name='register.html',
+             success_url='/',
+         ),
+         name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'),
+         name='login'),
+    path('logout/', views.logout_then_stay, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- revvviews/apps/main/views.py
+++ revvviews/apps/main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LogoutView
 
 
-def index(request):
-    return render(
-        request,
-        'index.html',
-        context={'message': 'Systems up and running!'},
-    )
+
+def logout_then_stay(request):
+    """Log out an authenticated user then redirect to the same page."""
+    current_page = request.META.get('HTTP_REFERER')
+    return LogoutView.as_view(next_page=current_page)(request)
+
