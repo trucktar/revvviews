be62f21ecebdaf350787958467fba5d65329364d
trucktar <57882685+trucktar@users.noreply.github.com>
2020-07-14 22:21:28 +0300

    Refactor url mapping for auth views

--- revvviews/apps/main/urls.py
+++ revvviews/apps/main/urls.py
@@ -1,23 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView
 from django.urls import path
-from django_registration.backends.one_step.views import RegistrationView
 
 from revvviews.apps.main import views
 
 urlpatterns = [
     path('', views.redirect_to_projects, name='home'),
     path('projects/', views.ProjectsView.as_view(), name='projects'),
-    path('projects/<title>', views.ProjectView.as_view(), name='project'),
-    path('<username>/', views.ProfileView.as_view(), name='profile'),
-    path('register/',
-         RegistrationView.as_view(
-             template_name='register.html',
-             success_url='/',
-         ),
-         name='register'),
-    path('login/', LoginView.as_view(template_name='login.html'),
-         name='login'),
+    path('projects/<title>/', views.ProjectView.as_view(), name='project'),
+    path('register/', views.RegistrationView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_then_stay, name='logout'),
+    path('<username>/', views.ProfileView.as_view(), name='profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
